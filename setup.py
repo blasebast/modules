db873db496c5d05f1414abb83b71f87d421d8cd3
Sebastian Blasiak <sebastian.blasiak@zoomint.com>
2017-03-21 10:25:51 +0100

    changes for pypi

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=requirements,
     license="GNU General Public License v3",
     zip_safe=False,
-    keywords='sebmodules',
+    keywords='sebmodules, sebastian, blasiak, logging, smoothlogging',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
